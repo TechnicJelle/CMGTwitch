f4f765acfb01548d760baa0c926d004b4b0ce23e
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-02-02 00:00:25 +0100

    Lecture monday!

--- lib/lecture_db.dart
+++ lib/lecture_db.dart
@@ -30,18 +30,23 @@ class Lecture {
 
 List<Course> courses = [
   Course(
-    "UI/UX Highly Very Advanced",
+    "UI/UX Advanced",
     [Audience.engineer, Audience.artist, Audience.designer],
     [
       Lecture(
-        "Lecture 01: Buttons and Navigation",
+        "Lecture 1: Buttons and Navigation",
         DateTime(2023, DateTime.january, 30, 9, 00),
         DateTime(2023, DateTime.january, 30, 11, 00),
       ),
       Lecture(
-        "Lecture 02: How to make your UI look good",
+        "Lecture 2: How to make your UI look good",
         DateTime(2023, DateTime.february, 2, 11, 00),
         DateTime(2023, DateTime.february, 2, 13, 00),
+      ),
+      Lecture(
+        "Lecture 3: How to make your UI not look like a 90s website",
+        DateTime(2023, DateTime.february, 6, 8, 30),
+        DateTime(2023, DateTime.february, 6, 10, 30),
       ),
     ],
   ),
@@ -50,9 +55,14 @@ List<Course> courses = [
     [Audience.engineer],
     [
       Lecture(
-        "Lecture 01: OpenGL and Shaders",
+        "Lec01: OpenGL and Shaders",
         DateTime(2023, DateTime.january, 31, 9, 00),
         DateTime(2023, DateTime.january, 31, 11, 00),
+      ),
+      Lecture(
+        "Lec02: Lighting and Shadows",
+        DateTime(2023, DateTime.february, 6, 10, 30),
+        DateTime(2023, DateTime.february, 6, 12, 30),
       ),
     ],
   ),
@@ -61,14 +71,19 @@ List<Course> courses = [
     [Audience.designer],
     [
       Lecture(
-        "Lecture 01: Where to put coins in your levels",
+        "Lecture 1: Where to put coins in your levels",
         DateTime(2023, DateTime.january, 31, 10, 00),
         DateTime(2023, DateTime.january, 31, 12, 00),
       ),
       Lecture(
-        "Lecture 02: How to design good levels and UI without looking like an absolute doofus",
+        "Lecture 2: How to design good levels and UI without looking like an absolute doofus",
         DateTime(2023, DateTime.february, 2, 10, 00),
         DateTime(2023, DateTime.february, 2, 12, 00),
+      ),
+      Lecture(
+        "Lecture 3: How to make your game look good with VFX",
+        DateTime(2023, DateTime.february, 6, 11, 00),
+        DateTime(2023, DateTime.february, 6, 12, 30),
       ),
     ],
   ),
@@ -77,14 +92,19 @@ List<Course> courses = [
     [Audience.artist],
     [
       Lecture(
-        "Lecture 01: Blender basics",
+        "Lec1: Blender basics",
         DateTime(2023, DateTime.february, 1, 9, 00),
         DateTime(2023, DateTime.february, 1, 11, 00),
       ),
       Lecture(
-        "Lecture 02: Blender advanced",
+        "Lec2: Blender advanced",
         DateTime(2023, DateTime.february, 2, 9, 00),
         DateTime(2023, DateTime.february, 2, 11, 00),
+      ),
+      Lecture(
+        "Lec3: Blender compositing",
+        DateTime(2023, DateTime.february, 6, 16, 00),
+        DateTime(2023, DateTime.february, 6, 18, 00),
       ),
     ],
   ),
