1d4111a89f4bf78232d7bd88538a425010826693
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-02-24 20:36:15 +0100

    Navigation rail improvements for thin screens
    Like smartphones

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -59,22 +59,24 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Row(
         children: [
-          NavigationRail(
-            extended: _extended,
-            elevation: 4,
-            destinations: _destinations,
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: (int index) =>
-                setState(() => _selectedIndex = index),
-            selectedLabelTextStyle: auto1NormalBody.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-              fontWeight: FontWeight.bold,
+          // Handle thin screens
+          if (MediaQuery.of(context).size.width > 500 || _extended)
+            NavigationRail(
+              extended: _extended,
+              elevation: 4,
+              destinations: _destinations,
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: (int index) =>
+                  setState(() => _selectedIndex = index),
+              selectedLabelTextStyle: auto1NormalBody.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+              unselectedLabelTextStyle: auto1NormalBody.copyWith(
+                color: Theme.of(context).colorScheme.onSurface.withOpacity(0.6),
+                fontWeight: FontWeight.normal,
+              ),
             ),
-            unselectedLabelTextStyle: auto1NormalBody.copyWith(
-              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.6),
-              fontWeight: FontWeight.normal,
-            ),
-          ),
           const VerticalDivider(thickness: 1, width: 1),
           Expanded(child: _currentPage),
         ],
